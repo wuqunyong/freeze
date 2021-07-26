b9a4a5e7464e18aca51b92404438b7c756ab32ae
freeze325 <wuqunyongproxy@gmail.com>
2021-07-26 13:59:03 +0800

    no message

--- server/test_server/logic/test_server.cpp
+++ server/test_server/logic/test_server.cpp
@@ -11,7 +11,7 @@
 namespace apie {
 
 
-	apie::status::Status TestServerMgr::init()
+apie::status::Status TestServerMgr::init()
 {
 	auto bResult = apie::CtxSingleton::get().checkIsValidServerType({ ::common::EPT_Test_Client });
 	if (!bResult)
@@ -20,8 +20,16 @@ namespace apie {
 	}
 
 
+	// PUBSUB
 	apie::pubsub::PubSubManagerSingleton::get().subscribe<::pubsub::LOGIC_CMD>(::pubsub::PUB_TOPIC::PT_LogicCmd, TestServerMgr::PubSub_logicCmd);
 	
+	// CMD
+	auto& cmd = LogicCmdHandlerSingleton::get();
+	cmd.init();
+	cmd.registerOnCmd("client", "client", TestServerMgr::Cmd_client);
+	cmd.registerOnCmd("auto_test", "auto_test", TestServerMgr::Cmd_autoTest);
+
+	// OPCODE
 	apie::service::ServiceHandlerSingleton::get().client.setDefaultFunc(TestServerMgr::handleDefaultOpcodes);
 
 	MockRole::registerPbOpcodeName(OP_MSG_RESPONSE_ACCOUNT_LOGIN_L, "login_msg.MSG_RESPONSE_ACCOUNT_LOGIN_L");
@@ -34,12 +42,12 @@ namespace apie {
 	return { apie::status::StatusCode::OK, "" };
 }
 
-	apie::status::Status TestServerMgr::start()
+apie::status::Status TestServerMgr::start()
 {
-		std::string ip = apie::CtxSingleton::get().getConfigs()->clients.socket_address.address;
-		uint16_t port = apie::CtxSingleton::get().getConfigs()->clients.socket_address.port_value;
-		uint16_t type = apie::CtxSingleton::get().getConfigs()->clients.socket_address.type;
-		uint32_t maskFlag = apie::CtxSingleton::get().getConfigs()->clients.socket_address.mask_flag;
+	std::string ip = apie::CtxSingleton::get().getConfigs()->clients.socket_address.address;
+	uint16_t port = apie::CtxSingleton::get().getConfigs()->clients.socket_address.port_value;
+	uint16_t type = apie::CtxSingleton::get().getConfigs()->clients.socket_address.type;
+	uint32_t maskFlag = apie::CtxSingleton::get().getConfigs()->clients.socket_address.mask_flag;
 
 	m_ptrClientProxy = apie::ClientProxy::createClientProxy();
 	auto connectCb = [](apie::ClientProxy* ptrClient, uint32_t iResult) {
@@ -55,7 +63,7 @@ namespace apie {
 	return { apie::status::StatusCode::OK, "" };
 }
 
-	apie::status::Status TestServerMgr::ready()
+apie::status::Status TestServerMgr::ready()
 {
 	std::stringstream ss;
 	ss << "Server Ready!";
@@ -115,208 +123,64 @@ void TestServerMgr::removeSerialNum(uint64_t iSerialNum)
 
 void TestServerMgr::PubSub_logicCmd(const std::shared_ptr<::pubsub::LOGIC_CMD>& msg)
 {
-	auto& command = *msg;
-
-	if (command.cmd() == "login")
+	auto handlerOpt = LogicCmdHandlerSingleton::get().findCb(msg->cmd());
+	if (!handlerOpt.has_value())
 	{
-		if (command.params_size() < 2)
-		{
-			return;
-		}
-
-		::login_msg::MSG_REQUEST_CLIENT_LOGIN request;
-		request.set_user_id(std::stoull(command.params()[0]));
-		request.set_version(std::stoi(command.params()[1]));
-		request.set_session_key(command.params()[2]);
-
-		TestServerMgrSingleton::get().m_ptrClientProxy->sendMsg(::apie::OP_MSG_REQUEST_CLIENT_LOGIN, request);
-
-		std::cout << "send|iSerialNum:" << TestServerMgrSingleton::get().m_ptrClientProxy->getSerialNum() << "|request:" << request.ShortDebugString() << std::endl;
+		return;
 	}
-	else if (command.cmd() == "echo")
+
+	handlerOpt.value()(*msg);
+}
+
+void TestServerMgr::Cmd_client(::pubsub::LOGIC_CMD& cmd)
+{
+	if (cmd.params_size() < 2)
 	{
-		if (command.params_size() < 1)
-		{
-			return;
-		}
-
-		uint64_t iCurMS = Ctx::getCurMilliseconds();
-
-		::login_msg::MSG_REQUEST_ECHO request;
-		request.set_value1(iCurMS);
-		request.set_value2(command.params()[0]);
-
-		TestServerMgrSingleton::get().m_ptrClientProxy->sendMsg(::apie::OP_MSG_REQUEST_ECHO, request);
-
-		std::cout << "send|iSerialNum:" << TestServerMgrSingleton::get().m_ptrClientProxy->getSerialNum() << "|request:" << request.ShortDebugString() << std::endl;
+		std::cout << "invalid params" << std::endl;
+		return;
 	}
-	else if (command.cmd() == "redis")
+
+	uint64_t iRoleId = std::stoull(cmd.params()[0]);
+	auto ptrMockRole = TestServerMgrSingleton::get().findMockRole(iRoleId);
+	if (ptrMockRole == nullptr)
 	{
-		if (command.params_size() < 2)
+		if (cmd.params()[1] == "account_login")
 		{
-			return;
-		}
+			ptrMockRole = MockRole::createMockRole(iRoleId);
+			ptrMockRole->start();
 
-
-		auto key = std::make_tuple(1, 1);
-		std::string sTable = command.params()[0];  //command.params()[0];
-		std::string field = command.params()[1];
-
-
-		if (sTable == "hs_player_summary")
-		{
-			auto redisClient = RedisClientFactorySingleton::get().getClient(key);
-			if (redisClient == nullptr)
-			{
-				return;
-			}
-
-			if (!redisClient->client().is_connected())
-			{
-				return;
-			}
-
-			auto cb = [sTable](cpp_redis::reply &reply) {
-				if (reply.is_error())
-				{
-					return;
-				}
-
-
-				if (reply.is_null())
-				{
-					return;
-				}
-
-				if (!reply.is_bulk_string())
-				{
-					return;
-				}
-
-				::role_msg::ROLE_SUMMARY summary;
-
-				std::string content = reply.as_string();
-				if (!summary.ParseFromString(content))
-				{
-					return;
-				}
-
-				std::cout << summary.DebugString() << std::endl;
-			};
-			redisClient->client().hget(sTable, field, cb);
-			redisClient->client().commit();
-		}
-		else if (sTable == "order_info")
-		{
-			field = command.params()[1] + "|" + command.params()[2];
-
-			auto redisClient = RedisClientFactorySingleton::get().getClient(key);
-			if (redisClient == nullptr)
-			{
-				return;
-			}
-
-			if (!redisClient->client().is_connected())
-			{
-				return;
-			}
-
-			auto cb = [sTable](cpp_redis::reply &reply) {
-				if (reply.is_error())
-				{
-					return;
-				}
-
-
-				if (reply.is_null())
-				{
-					return;
-				}
-
-				if (!reply.is_bulk_string())
-				{
-					return;
-				}
-
-				//::gm_msg::ORDER_INFO order;
-
-				//std::string content = reply.as_string();
-				//if (!order.ParseFromString(content))
-				//{
-				//	return;
-				//}
-
-				//std::cout << order.DebugString() << std::endl;
-			};
-			redisClient->client().hget(sTable, field, cb);
-			redisClient->client().commit();
+			TestServerMgrSingleton::get().addMockRole(ptrMockRole);
 		}
 		else
 		{
-
-		}
-
-	}
-	else if (command.cmd() == "client")
-	{
-		if (command.params_size() < 2)
-		{
-			std::cout << "invalid params" << std::endl;
+			std::cout << "not login|account:" << iRoleId << std::endl;
 			return;
 		}
-
-		uint64_t iRoleId = std::stoull(command.params()[0]);
-		auto ptrMockRole = TestServerMgrSingleton::get().findMockRole(iRoleId);
-		if (ptrMockRole == nullptr)
-		{
-			if (command.params()[1] == "account_login")
-			{
-				ptrMockRole = MockRole::createMockRole(iRoleId);
-				ptrMockRole->start();
-
-				TestServerMgrSingleton::get().addMockRole(ptrMockRole);
-			}
-			else
-			{
-				std::cout << "not login|account:" << iRoleId << std::endl;
-				return;
-			}
-		}
-
-		::pubsub::LOGIC_CMD newMsg;
-		for (int i = 1; i < command.params().size(); i++)
-		{
-			if (i == 1)
-			{
-				newMsg.set_cmd(command.params()[i]);
-			}
-			else
-			{
-				auto ptrAdd = newMsg.add_params();
-				*ptrAdd = command.params()[i];
-			}
-		}
-		ptrMockRole->pushMsg(newMsg);
 	}
-	else if (command.cmd() == "auto_test")
+
+	::pubsub::LOGIC_CMD newMsg;
+	for (int i = 1; i < cmd.params().size(); i++)
 	{
-		std::cout << "start auto_test" << std::endl;
-
-		TestRunnerMgrSingleton::get().init();
-		TestRunnerMgrSingleton::get().run();
+		if (i == 1)
+		{
+			newMsg.set_cmd(cmd.params()[i]);
+		}
+		else
+		{
+			auto ptrAdd = newMsg.add_params();
+			*ptrAdd = cmd.params()[i];
+		}
 	}
-
+	ptrMockRole->pushMsg(newMsg);
 }
 
-//void TestServerMgr::handleResponseClientLogin(uint64_t iSerialNum, const ::login_msg::MSG_RESPONSE_CLIENT_LOGIN& response)
-//{
-//	std::cout << "recv|iSerialNum:" << iSerialNum << "|response:" << response.ShortDebugString() << std::endl;
-//}
-//
-//void TestServerMgr::handleResponseEcho(uint64_t iSerialNum, const ::login_msg::MSG_RESPONSE_ECHO& response)
-//{
-//	std::cout << "recv|iSerialNum:" << iSerialNum << "|response:" << response.ShortDebugString() << std::endl;
-//}
+void TestServerMgr::Cmd_autoTest(::pubsub::LOGIC_CMD& cmd)
+{
+	std::cout << "start auto_test" << std::endl;
+
+	TestRunnerMgrSingleton::get().init();
+	TestRunnerMgrSingleton::get().run();
+}
 
 void TestServerMgr::handleDefaultOpcodes(uint64_t serialNum, uint32_t opcodes, const std::string& msg)
 {
--- server/test_server/logic/test_server.h
+++ server/test_server/logic/test_server.h
@@ -18,35 +18,40 @@
 
 namespace apie {
 
-	class TestServerMgr
-	{
-	public:
-		apie::status::Status init();
-		apie::status::Status start();
-		apie::status::Status ready();
-		void exit();
+class TestServerMgr
+{
+public:
+	apie::status::Status init();
+	apie::status::Status start();
+	apie::status::Status ready();
+	void exit();
 
-		void addMockRole(std::shared_ptr<MockRole> ptrMockRole);
-		std::shared_ptr<MockRole> findMockRole(uint64_t iRoleId);
-		void removeMockRole(uint64_t iRoleId);
+	void addMockRole(std::shared_ptr<MockRole> ptrMockRole);
+	std::shared_ptr<MockRole> findMockRole(uint64_t iRoleId);
+	void removeMockRole(uint64_t iRoleId);
 
-		void addSerialNumRole(uint64_t iSerialNum, uint64_t iRoleId);
-		std::optional<uint64_t> findRoleIdBySerialNum(uint64_t iSerialNum);
-		void removeSerialNum(uint64_t iSerialNum);
+	void addSerialNumRole(uint64_t iSerialNum, uint64_t iRoleId);
+	std::optional<uint64_t> findRoleIdBySerialNum(uint64_t iSerialNum);
+	void removeSerialNum(uint64_t iSerialNum);
 
-	public:
-		static void PubSub_logicCmd(const std::shared_ptr<::pubsub::LOGIC_CMD>& msg);
-		static void handleDefaultOpcodes(uint64_t serialNum, uint32_t opcodes, const std::string& msg);
+public:
+	static void PubSub_logicCmd(const std::shared_ptr<::pubsub::LOGIC_CMD>& msg);
 
-		//static void handleResponseClientLogin(uint64_t iSerialNum, const ::login_msg::MSG_RESPONSE_CLIENT_LOGIN& response);
-		//static void handleResponseEcho(uint64_t iSerialNum, const ::login_msg::MSG_RESPONSE_ECHO& response);
-	public:
-		std::shared_ptr<ClientProxy> m_ptrClientProxy;
+	static void Cmd_client(::pubsub::LOGIC_CMD& cmd);
+	static void Cmd_autoTest(::pubsub::LOGIC_CMD& cmd);
 
-		std::map<uint64_t, std::shared_ptr<MockRole>> m_mockRole;
-		std::map<uint64_t, uint64_t> m_serialNumRole;
-	};
+	static void handleDefaultOpcodes(uint64_t serialNum, uint32_t opcodes, const std::string& msg);
 
 
-	using TestServerMgrSingleton = ThreadSafeSingleton<TestServerMgr>;
+public:
+	std::shared_ptr<ClientProxy> m_ptrClientProxy;
+
+	std::map<uint64_t, std::shared_ptr<MockRole>> m_mockRole;
+	std::map<uint64_t, uint64_t> m_serialNumRole;
+};
+
+
+using TestServerMgrSingleton = ThreadSafeSingleton<TestServerMgr>;
+
+
 }
