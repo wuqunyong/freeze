64a49d8da42b4f11b8997b537710b90f76789e54
freeze325 <wuqunyongproxy@gmail.com>
2021-08-09 11:52:37 +0800

    no message

--- server/test_server/test_client.cpp
+++ server/test_server/test_client.cpp
@@ -12,39 +12,8 @@
 #include "service_init.h"
 
 
-template <size_t I = 0, typename... Ts>
-constexpr void printTuple(std::tuple<Ts...> tup, std::map<uint32_t, uint32_t> index)
-{
-	// If we have iterated through all elements
-	if
-		constexpr (I == sizeof...(Ts))
-	{
-		// Last case, if nothing is left to
-		// iterate, then exit the function
-		return;
-	}
-	else {
-		// Print the tuple and go to next element
-		if (index[I] > 0)
-		{
-			std::cout << "index:" << I  << "|" << std::get<I>(tup) << " ";
-		}
-
-		// Going for next element.
-		printTuple<I + 1>(tup, index);
-	}
-}
-
 int main(int argc, char **argv)
 {
-	std::tuple<std::string, std::string, std::string> tup("Geeks","for","Geeks");
-	std::map<uint32_t, uint32_t> index;
-	index[0] = 1;
-	index[2] = 1;
-	printTuple(tup, index);
-
-
-
 	if (argc != 2)
 	{
 		PANIC_ABORT("usage: exe <ConfFile>, Expected: %d, got: %d", 2, argc);
