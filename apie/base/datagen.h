2429ed595939112b159643cd3c5060ab618c45e8
freeze325 <wuqunyongproxy@gmail.com>
2022-04-01 17:34:44 +0800

    no message

--- apie/base/datagen.h
+++ apie/base/datagen.h
@@ -13,20 +13,7 @@
 namespace apie {
 namespace base {
 
-std::string RandomString(size_t length) {
-  constexpr char kCharSet[] =
-      "0123456789"
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      "abcdefghijklmnopqrstuvwxyz";
-  std::random_device rnd_device;
-  std::mt19937 mersenne_engine{rnd_device()};  // Generates random integers
-  std::uniform_int_distribution<> distrib(0, sizeof(kCharSet) - 1);
-
-  auto randchar = [&]() -> char { return kCharSet[distrib(mersenne_engine)]; };
-  std::string str(length, 0);
-  std::generate_n(str.begin(), length, randchar);
-  return str;
-}
+extern std::string RandomString(size_t length);
 
 }
 }
