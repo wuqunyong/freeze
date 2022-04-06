16f431afa3854c2e6459479e11e91d099e8c0451
freeze325 <wuqunyongproxy@gmail.com>
2022-04-06 11:19:31 +0800

    no message

---
+++ GTestSuite/gtest_demo1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+#include "gtest/gtest.h"
+
+TEST(Demo1, Increment) {
+	EXPECT_EQ(0,0);
+}
