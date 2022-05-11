1b60c69a0ce558be52b2cad943fa803eb765d047
qunyongwu <wuqunyongproxy@gmail.com>
2022-05-11 20:15:17 +0800

    no message

---
+++ pb_msg/init.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../pb_msg/business/login_msg.pb.h"
+#include "../pb_msg/business/login.pb.h"
+#include "../pb_msg/business/userinfo.pb.h"
+#include "../pb_msg/business/map.pb.h"
+#include "../pb_msg/business/db.pb.h"
+#include "../pb_msg/business/decree.pb.h"
--- server/test_server/logic/mock_role.h
+++ server/test_server/logic/mock_role.h
@@ -12,12 +12,7 @@
 
 #include "apie.h"
 
-#include "../../pb_msg/business/login_msg.pb.h"
-#include "../../pb_msg/business/login.pb.h"
-#include "../../pb_msg/business/userinfo.pb.h"
-#include "../../pb_msg/business/map.pb.h"
-#include "../../pb_msg/business/db.pb.h"
-#include "../../pb_msg/business/decree.pb.h"
+#include "../../pb_msg/init.h"
 
 
 namespace apie {
