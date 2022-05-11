fb58d70821b20ede65735dc5e4dbd4865c9b3c81
qunyongwu <wuqunyongproxy@gmail.com>
2022-05-11 20:17:44 +0800

    no message

--- pb_msg/init.h
+++ pb_msg/init.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../pb_msg/business/login_msg.pb.h"
 #include "../pb_msg/business/login.pb.h"
 #include "../pb_msg/business/userinfo.pb.h"
 #include "../pb_msg/business/map.pb.h"
